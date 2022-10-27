2b078c4628b732d85be00cce8ed657b90e74157f
Diana Lauzhyna <dlauzhyna@gmail.com>
2022-10-28 02:53:12 +0300

    changes

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { Button } from './FeedbackOptions.styled';
 
-export function FeedbackOptions({ options, onLeaveFeedback, type }) {
+export function FeedbackOptions({ options, onLeaveFeedback }) {
   return Object.keys(options).map(dataSetElement => (
-    <Button type={type} key={dataSetElement} onClick={onLeaveFeedback}>
+    <Button key={dataSetElement} onClick={onLeaveFeedback}>
       {dataSetElement}
     </Button>
   ));
@@ -12,5 +12,4 @@ export function FeedbackOptions({ options, onLeaveFeedback, type }) {
 FeedbackOptions.propTypes = {
   options: PropTypes.object.isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
-  type: PropTypes.string.isRequired,
 };
